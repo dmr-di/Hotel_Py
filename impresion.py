57c47ed366584d94336b0959202093c296627348
mirotic13 <dani-15-94@hotmail.com>
2020-01-13 09:49:06 +0100

    Ampliación módulo impresión

--- impresion.py
+++ impresion.py
@@ -6,21 +6,28 @@ def basico():
     try:
         bill = canvas.Canvas("prueba.pdf", pagesize=A4)
         text1 = "Bienvenido a nuestro hotel"
+        text2 = "CIF = 000000000A"
         bill.setFont("Helvetica-Bold", size=16)
         bill.drawImage("./img/hotel.png", 475, 680, width=64, height=64)
         bill.drawString(240, 780, "HOTEL LITE")
         bill.setFont("Times-Italic", size=10)
         bill.drawString(235, 765, text1)
-        bill.line(50, 670,540,670)
-        bill.showPage()
-        bill.save()
-        dir = os.getcwd()
-        os.system("/usr/bin/xdg-open " + dir + "/prueba.pdf")
+        bill.drawString(250, 755, text2)
+        bill.line(50, 670, 540, 670)
+        textpie = "Hotel Lite, CIF = 000000000A, Tlfn = 986000000, e-mail = info@hotellite.com"
+        bill.setFont("Times-Italic", size=8)
+        bill.drawString(280, 20, textpie)
+        bill.line(50, 30, 540, 30)
+        return bill
     except:
         print("Error en básico")
 
 def factura():
     try:
-        basico()
+        bill = basico()
+        bill.showPage()
+        bill.save()
+        dir = os.getcwd()
+        os.system("/usr/bin/xdg-open " + dir + "/prueba.pdf")
     except:
         print("Error en módulo factura")
